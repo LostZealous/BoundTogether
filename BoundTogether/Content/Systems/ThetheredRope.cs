30a0bb1c4919dbb39fae1a405233cb96b7f1428c
LostZealous <158816781+LostZealous@users.noreply.github.com>
2024-12-31 16:56:41 -0600

    Updated ThetheredRope.cs
    Fixed the 18+ Errors I made... Whoops

--- BoundTogether/Content/Systems/ThetheredRope.cs
+++ BoundTogether/Content/Systems/ThetheredRope.cs
@@ -1,23 +1,41 @@
-using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 
-public void DrawRope()
+namespace BoundTogether.Content.Systems
 {
-    Vector2 start = player1.Center;
-    Vector2 end = player2.Center;
-
-    
-    Texture2D tongueTexture = TextureAssets.Projectile[ProjectileID.Tongue].Value;
-
-    Vector2 direction = end - start;
-    float rotation = (float)Math.Atan2(direction.Y, direction.X);
-    float length = direction.Length();
-
-    
-    for (float i = 0; i < length; i += tongueTexture.Height * 0.5f)
+    public class TetheredRope
     {
-        Vector2 position = start + direction * (i / length);
-        Main.spriteBatch.Draw(tongueTexture, position - Main.screenPosition, null, Color.White,
-                              rotation, new Vector2(tongueTexture.Width * 0.5f, tongueTexture.Height * 0.5f),
-                              1f, SpriteEffects.None, 0f);
+        private Player player1;
+        private Player player2;
+
+        public TetheredRope(Player player1, Player player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public void DrawRope()
+        {
+            Vector2 start = player1.Center;
+            Vector2 end = player2.Center;
+            CreateDustRope(start, end);
+        }
+
+        private void CreateDustRope(Vector2 start, Vector2 end)
+        {
+            Vector2 direction = end - start;
+            float distance = direction.Length();
+            int segments = (int)(distance / 8); 
+            direction.Normalize();
+
+            for (int i = 0; i <= segments; i++)
+            {
+                Vector2 position = start + direction * (i * 8); 
+                Dust dust = Dust.NewDustPerfect(position, DustID.Smoke, Vector2.Zero, 100, Color.White, 1.2f);
+                dust.noGravity = true; 
+                dust.fadeIn = 1f; 
+            }
+        }
     }
 }
