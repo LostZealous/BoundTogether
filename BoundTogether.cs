b6ffa507e88dfa574e31bcd24a772d19014edec7
LostZealous <158816781+LostZealous@users.noreply.github.com>
2025-01-05 19:41:27 -0600

    uyes

---
+++ BoundTogether.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace BoundTogether
+{
+    public class BoundTogether : Mod
+    {
+        // This class can be left empty unless additional mod-wide features are implemented.
+    }
+}
