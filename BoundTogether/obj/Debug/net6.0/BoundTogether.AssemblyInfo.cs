06efef4ed2f8d27beb7c0c8526f0b7f8acab2c36
cory <corysands12@gmail.com>
2025-01-05 19:51:04 -0600

    Upgrade Project Version

--- BoundTogether/obj/Debug/net6.0/BoundTogether.AssemblyInfo.cs
+++ BoundTogether/obj/Debug/net6.0/BoundTogether.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoundTogether")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0ea0357f3e3d6adf065f7fa130c25799839272")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoundTogether")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoundTogether")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
