b1b3d981afd1145268b39ff505295d6fb8ea5f30
LostZealous <cw11142003@gmail.com>
2025-01-05 22:08:12 -0600

    just hear me out

--- KillOnPlayerDeath.cs
+++ KillOnPlayerDeath.cs
@@ -2,48 +2,60 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Terraria.DataStructures;
-using System.IO;
+using System.Collections.Generic;
 
-namespace BoundTogether;
-public class KillOnPlayerDeath : ModSystem
+namespace BoundTogether
 {
-    public override void PostUpdatePlayers()
+    public class KillOnPlayerDeath : ModSystem
     {
-        foreach (Player player in Main.player)
+        private HashSet<int> processedPlayers = new HashSet<int>();
+
+        public override void PostUpdatePlayers()
         {
-            if (player.active && player.dead) // Check if the player is dead
+            foreach (Player player in Main.player)
             {
-                HandleDeath(player);
+                if (player.active)
+                {
+                    if (player.dead && !processedPlayers.Contains(player.whoAmI))
+                    {
+                        HandleDeath(player);
+                        processedPlayers.Add(player.whoAmI);
+                    }
+                    else if (!player.dead && processedPlayers.Contains(player.whoAmI))
+                    {
+                        processedPlayers.Remove(player.whoAmI);
+                    }
+                }
             }
         }
-    }
 
-    private void HandleDeath(Player triggeringPlayer)
-    {
-        if (Main.netMode == NetmodeID.Server)
+        private void HandleDeath(Player triggeringPlayer)
         {
-            ModPacket packet = Mod.GetPacket();
-            packet.Write((byte)MessageType.PlayerDeathTriggered);
-            packet.Write(triggeringPlayer.whoAmI);
-            packet.Send();
+            if (Main.netMode == NetmodeID.Server)
+            {
+                ModPacket packet = Mod.GetPacket();
+                packet.Write((byte)MessageType.PlayerDeathTriggered);
+                packet.Write(triggeringPlayer.whoAmI);
+                packet.Send();
+            }
+
+            KillAllPlayers(triggeringPlayer);
         }
 
-        KillAllPlayers(triggeringPlayer);
-    }
-
-    public static void KillAllPlayers(Player triggeringPlayer)
-    {
-        foreach (Player player in Main.player)
+        public static void KillAllPlayers(Player triggeringPlayer)
         {
-            if (player.active && !player.dead && player.whoAmI != triggeringPlayer.whoAmI)
+            foreach (Player player in Main.player)
             {
-                player.KillMe(PlayerDeathReason.ByCustomReason($"{triggeringPlayer.name}'s death caused {player.name} to perish!"), 9999, 0);
+                if (player.active && !player.dead && player.whoAmI != triggeringPlayer.whoAmI)
+                {
+                    player.KillMe(PlayerDeathReason.ByCustomReason($"{triggeringPlayer.name}'s death caused {player.name} to perish!"), 9999, 0);
+                }
             }
         }
-    }
 
-    public enum MessageType
-    {
-        PlayerDeathTriggered
+        public enum MessageType
+        {
+            PlayerDeathTriggered
+        }
     }
 }
